e77bb832e585066b4cfeca58c60db080c3c7255c
Seungyeol01 <marudev4905@gmail.com>
2024-05-17 17:03:31 +0900

    관리자 페이지 아이콘 추가 및 사이드 메뉴 언어 변경 버튼 추가

--- src/assets/js/adm.js
+++ src/assets/js/adm.js
@@ -10,5 +10,15 @@ window.onload = function(){
 $(document).ready(function(){
   $(".adm_menu_btn").on('click', function(){
     $(".adm_menu").toggleClass("hide");
+    if($(".lang_btn").hasClass("on")){
+      $(".lang_btn").removeClass("on");
+      $(".side_lang_list").removeClass("on");
+    }
+  });
+
+  $(".lang_btn").on("click", function() {
+    if($(".adm_menu").hasClass("hide")){
+      $(".adm_menu").removeClass("hide");
+    }
   });
 });
