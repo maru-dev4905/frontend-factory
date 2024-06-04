a40b4beae6f04791838d5d029c440326f895637b
Seungyeol01 <marudev4905@gmail.com>
2024-06-04 13:59:21 +0900

    playground

--- src/assets/js/playground.js
+++ src/assets/js/playground.js
@@ -2,39 +2,20 @@ import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(MotionPathPlugin);
-gsap.to(".dot1", {
-  duration: 5,
-  ease: "linear",
-  immediateRender: true,
-  repeat: -1,
-  motionPath: {
-    path: "#line1",
-    align: "#line1",
-    alignOrigin: [0.5, 0.5],
-    loop: true,
-    start: 0,
-  }
-});
-gsap.to(".dot1", {
-  motionPath: {end: 1}
-});
 
-gsap.to(".dot2", {
-  duration: 5,
-  ease: "linear",
-  immediateRender: true,
-  repeat: -1,
-  motionPath: {
-    path: "#line2",
-    align: "#line2",
-    alignOrigin: [0.5, 0.5],
-    loop: true,
-    start: 0,
-  }
+gsap.utils.toArray(".dot").forEach((dot, i) => {
+  console.log(dot, i);
+  gsap.to(dot, {
+    duration: Math.floor(Math.random() * 10 + 5),
+    ease: "linear",
+    immediateRender: true,
+    repeat: -1,
+    motionPath: {
+      path: "#line" + (i + 1),
+      align: "#line" + (i + 1),
+      alignOrigin: [0.5, 0.5],
+      loop: true,
+      start: 0,
+    }
+  });
 });
-gsap.to(".dot2", {
-  motionPath: {end: 1}
-});
-
-MotionPathHelper.create(".dot1");
-MotionPathHelper.create(".dot2");
